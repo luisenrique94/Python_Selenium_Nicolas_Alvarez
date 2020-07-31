639a547e804e0fb6ed0a650a10855c34c7c2753b
luis chavez <luisenriquechavezporroa@gmail.com>
2020-07-31 11:49:08 -0500

    Se subio el archivo

--- Test/Subir_Archivo.py
+++ Test/Subir_Archivo.py
@@ -1,8 +1,22 @@
 import unittest
-
+from  selenium import webdriver
+import time
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            executable_path="C:\\Users\\Luis\\Desktop\\Python_Selenium_Nicolas_Alvarez\\Drivers\\chromedriver.exe")
+        self.driver.maximize_window()
     def test_something(self):
-        self.assertEqual(True, False)
+        driver = self.driver
+        driver.get("https://mdbootstrap.com/plugins/jquery/file-upload")
+        time.sleep(4)
+        self.driver.find_element_by_id("input-file-now-custom-2").send_keys("C:\\Users\\Luis\\Desktop\\SPRING LOVE\\20191228_180018.jpg")
+        time.sleep(4)
+
+    def tearDown(self):
+        self.driver.close()
+
 
 if __name__ == '__main__':
     unittest.main()
