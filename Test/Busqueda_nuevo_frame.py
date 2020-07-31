f5a7e07906b6d8ce9416b7a65313529e44da9e19
luis chavez <luisenriquechavezporroa@gmail.com>
2020-07-31 16:23:11 -0500

    se agrearon dos archivos

--- Test/Busqueda_nuevo_frame.py
+++ Test/Busqueda_nuevo_frame.py
@@ -1,8 +1,24 @@
 import unittest
+from  selenium import webdriver
+import time
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            executable_path="C:\\Users\\Luis\\Desktop\\Python_Selenium_Nicolas_Alvarez\\Drivers\\chromedriver.exe")
+        self.driver.maximize_window()
     def test_something(self):
-        self.assertEqual(True, False)
+        driver = self.driver
+        driver.get("http://google.com")
+        time.sleep(4)
+        click1 = driver.find_element_by_xpath("//*[@id='gbwa']/div/a")
+        time.sleep(3)
+        click1.click()
+        time.sleep(2)
+        driver.switch_to.frame(driver.find_element_by_xpath())
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- Test/Click_derecho.py
+++ Test/Click_derecho.py
@@ -1,8 +1,27 @@
 import unittest
+from  selenium import webdriver
+from  selenium.webdriver.common.by import  By
+from selenium.webdriver import ActionChains
+import time
+
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            executable_path="C:\\Users\\Luis\\Desktop\\Python_Selenium_Nicolas_Alvarez\\Drivers\\chromedriver.exe")
+        self.driver.maximize_window()
+
     def test_something(self):
-        self.assertEqual(True, False)
+        self.driver.get("http://nicolasalvarez.com")
+        time.sleep(4)
+        youtube = self.driver.find_element_by_xpath("//div[@class='inner-wrap clearfix']//i[@class='fa fa-youtube']")
+        actions = ActionChains(self.driver)
+        actions.context_click(youtube).perform()
+        time.sleep(4)
+
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == '__main__':
     unittest.main()
