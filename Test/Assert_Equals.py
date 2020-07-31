a51bc18fc51181f2bee964f8947d666a738e924a
luis chavez <luisenriquechavezporroa@gmail.com>
2020-07-31 10:22:19 -0500

    se agrego  el archivo Assert_Equals

---
+++ Test/Assert_Equals.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path="C:\\Users\\Luis\\Desktop\\Python_Selenium_Nicolas_Alvarez\\Drivers\\chromedriver.exe")
+        self.driver.maximize_window()
+       
+    def test_something(self):
+        driver = self.driver
+        driver.get("https://www.google.com/")
+        titulo = driver.title
+        print(titulo)
+        time.sleep(5)
+        self.assertEqual("Google", titulo,"el titulo de la pagina no es igual ")
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
