6e09f413fcff732a4fcbbd18200799e0d9da5f97
luis chavez <luisenriquechavezporroa@gmail.com>
2020-07-31 11:57:59 -0500

    se agrego el archivo doble_click

---
+++ Test/Doble_click.py
@@ -0,0 +1,27 @@
+import unittest
+from  selenium import webdriver
+from  selenium.webdriver.common.by import  By
+from selenium.webdriver import ActionChains
+import time
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            executable_path="C:\\Users\\Luis\\Desktop\\Python_Selenium_Nicolas_Alvarez\\Drivers\\chromedriver.exe")
+        self.driver.maximize_window()
+    def test_something(self):
+        self.driver.get("http://nicolasalvarez.com")
+        time.sleep(4)
+        dobleclick= self.driver.find_element_by_xpath("//*[@id='recent-posts-2']/h3/span")
+        actions = ActionChains(self.driver)
+        actions.double_click(dobleclick).perform()
+        time.sleep(4)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
